bd3ed0ac4ae12538d58effc0a08555f36593f762
LeonidAlekseev <55627946+LeonidAlekseev@users.noreply.github.com>
2020-03-20 20:59:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,42 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask
-from flask import request
-from flask import jsonify
-from flask_sslify import SSLify
-import requests
-import random
-import vk_api
-import json
-import os
+from flask import Flask # VKBot
+from flask import request # VKBot
+from flask import jsonify # VKBot
+from flask_sslify import SSLify # VKBot
+import requests # VKBot & YandexPars
+import random # VKBot
+import vk_api # VKBot
+import json # VKBot
+import os # VKBot & Heroku
+from lxml import html # YandexPars
 
+# YandexPars
+def get_tags(photo_url):
+    yandex_search = "https://yandex.ru/images/search?rpt=imageview&url="+photo_url
+    html_url = requests.get(yandex_search)
+    tree_html = html.fromstring(html_url.text.encode('UTF-8'))
+    tags_tree = tree_html.xpath('//a[contains(@class, "tags__tag")]') # a теги с атрибутом clas равным "..."
+    tags = [i.text for i in tags_tree]
+    return tags
+
+def get_result(photo_url):
+    tags = get_tags(photo_url)
+    if tags == []:
+        time.sleep(3)
+        tags = get_tags(photo_url)
+        if tags == []:
+            message = "что-то непонятное"
+    tags = ", ".join(tags)
+    message = "Кажется, на картинке " + tags + "."
+    return message
 
 # VkApi
 token = str(os.environ.get("TOKEN"))
 confirmation_token = str(os.environ.get("CONFIRMATION_TOKEN"))
 vk = vk_api.VkApi(token=token, api_version='5.89')
-# -VkApi
+
 # Flask
 app = Flask(__name__)
 sslify = SSLify(app)
@@ -33,16 +53,11 @@ def index():
                 attachments = object["attachments"]
                 if attachments != []: # Проверяем есть ли прикрепления
                     if attachments[0]["type"] == "photo": # Проверяем есть ли фото в прикреплении
-                        print("1")
                         id = object["peer_id"]
-                        print("2")
                         photo = attachments[0]["photo"]
-                        print("3")
                         largest_photo = photo["sizes"][-1]
-                        print("4")
                         url_photo = largest_photo["url"]
-                        print("5")
-                        vk.method("messages.send", {"peer_id": id, "message": url_photo, "random_id": random.randint(1, 2147483647)})
+                        vk.method("messages.send", {"peer_id": id, "message": get_result(url_photo), "random_id": random.randint(1, 2147483647)})
                 # ----- Обработка команд -----
                 # body = object["text"]
                 # if body.lower() == "привет":
@@ -53,8 +68,8 @@ def index():
     elif request.method == 'GET':
         return '<h1>VKBot working now!</h1>'
     return 'OK'
-# -Flask
 
+# SystemDebug
 if __name__ == '__main__':
     app.debug = True
     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
